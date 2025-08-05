011255e5acbade18078d26ce0d35be41d7eb3a65
0xEthan <ethanzhang@creditease.cn>
2025-08-05 18:08:25 +0800

    feat: faucet TokenList add stETH

--- src/common/FaucetConfig.js
+++ src/common/FaucetConfig.js
@@ -2,13 +2,6 @@ import { normalizeFaucetConfigKeys } from "../utils/Utils";
 
 // token name should toLowerCase
 const faucetConfigRaw = {
-  // 5: {
-  //   // Goerli
-  //   faucet: "0xC1f60B81c6dAb5BE517a53983708488F9978D0Eb",
-  //   ygio: "0xd042eF5cF97c902bF8F53244F4a81ec4f8E465Ab",
-  //   ygme: "0x28D1bC817DE02C9f105A6986eF85cB04863C3042",
-  //   usdt: "0x965A558b312E288F5A77F851F7685344e1e73EdF"
-  // },
   97: {
     // TBSC
     faucet: "0x1ef1b1405dCda2375Fc8430DE5560016F0D8DBe0",
@@ -22,7 +15,7 @@ const faucetConfigRaw = {
     // ygio: "0x5Bb9dE881543594D17a7Df91D62459024c4EEf02",
     ygme: "0x709B78B36b7208f668A3823c1d1992C0805E4f4d",
     usdt: "0x590dcA422b660071F978E5A69851A18529B45415",
-    wstETH: "0xa9686DAcB46cb07ab14290dAF1b0B4feeAD99366",
+    stETH: "0x1d1d95791Dcc3fa1714Be0c8D81B81b6e84B3C14",
     usdc: "0x6125c3dA36152A5e68a5AC5686A09d6244690bcf"
   }
 };
@@ -32,7 +25,8 @@ export const faucetConfig = normalizeFaucetConfigKeys(faucetConfigRaw);
 export const faucetTokenList = [
   // { label: "YGIO", faucetAmount: 1000 },
   { label: "USDT", faucetAmount: 1000 },
-  { label: "USDC", faucetAmount: 1000 }
+  { label: "USDC", faucetAmount: 1000 },
+  { label: "stETH", faucetAmount: 100 }
 ];
 
 export const getFaucetTokenAddress = (chainId, tokenName) => {
